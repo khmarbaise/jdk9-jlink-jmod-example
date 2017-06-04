2240a7e1a027e7b318b9e4bca403ba10a94ec073
Karl Heinz Marbaise <khmarbaise@apache.org>
2017-06-04 21:13:01 +0200

    Create a JAR file with module-info.class

---
+++ jar-create.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.9.0_ea+172.jdk/Contents/Home
+JAR=${JAVA_HOME}/bin/jar
+JMODS=${JAVA_HOME}/jmods
+
+mkdir -p target
+$JAR \
+  --verbose \
+  --create \
+  --file target/result.jar \
+  --module-version 1.0-SNAPHSHOT \
+  --module-path $JMODS:target/classes \
+  -C target/classes org 
