c31bcee8afcff33ec9d55eccd380b9bb44819a5a
Karl Heinz Marbaise <khmarbaise@apache.org>
2017-06-06 20:01:09 +0200

    Show the module name from the CLI call.

--- maven-example/mod-2/src/main/java/com/soebes/example/nine/two/Main.java
+++ maven-example/mod-2/src/main/java/com/soebes/example/nine/two/Main.java
@@ -1,12 +1,16 @@
 package com.soebes.example.nine.two;
 
-public class Main {
-  public static void main(String[] args) {
-    System.out.println("Main from Java 9");
-    if ( args.length > 0 ) {
-      for (String arg : args) {
-        System.out.println (" Arg: '" + arg + "'");
-      }
+public class Main
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Main two from Java 9" );
+        if ( args.length > 0 )
+        {
+            for ( String arg : args )
+            {
+                System.out.println( " Arg: '" + arg + "'" );
+            }
+        }
     }
-  }
 }
