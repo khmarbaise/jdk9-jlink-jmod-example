1c1f673d8cc7b24c62ef64c358edce6d115ae523
Karl Heinz Marbaise <khmarbaise@apache.org>
2018-03-04 15:51:18 +0100

    Get working on Alpha-2 SNAPSHOT

--- maven-example/mod-jlink/src/main/java/module-info.java
+++ maven-example/mod-jlink/src/main/java/module-info.java
@@ -1,4 +1,5 @@
 module org.apache {
   requires java.base;
-  exports org.apache.j9;
+/*  exports org.apache.j9; */
+  exports com.soebes.example.nine;
 }
---
+++ mod-jar/src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module com.soebes.nine.jar {
+  requires java.base;
+  exports com.soebes.example.nine.jar;
+}
